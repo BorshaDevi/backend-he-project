d026752c810535f0e6f2c793fdaf490d770fc945
unknown <borshadevi27@gmail.com>
2025-01-13 22:03:44 +0600

    added login funcaliti

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const bcrypt  = require('bcryptjs');
 require('dotenv').config()
 const jwt = require('jsonwebtoken');
 const app = express()
@@ -36,26 +37,41 @@ app.post('/jwt',async (req,res) =>{
 
 app.post('/loginUsers',async(req,res)=>{
   const loginUser=req.body
+  // console.log(loginUser ,'users')
   const name=loginUser.name
   const password=loginUser.password
-  const query={name:name , password :password}
+  const query={ name :name}
   const searchIngUser=await userCollection.findOne(query)
+  console.log(searchIngUser.password ,"hash password")
+  const userCollectionPassword=bcrypt.compareSync(password, searchIngUser.password)
+  // console.log(userCollectionPassword)
   if(!searchIngUser){
     return res.send("Unauthorize")
   }
-  const loginUserData=await loginUserCollection.insertOne(loginUser)
+  const hashPassword= bcrypt.hashSync(password ,10)
+  const login={
+    name:name,
+    password:hashPassword,
+  }
+  const loginUserData=await loginUserCollection.insertOne(login)
+  // console.log(loginUserData ,"login done")
   res.send("Login successfully")
 })
 
 app.post('/users',async(req,res)=>{
   const user=req.body
   const email=user.email
+  const name=user.name
   const query={email : email}
+  const password=user.password
+  const role=user.role
+  const hashPassword= bcrypt.hashSync(password ,10)
+  const userAllData={name:name,password:hashPassword,email :email,role :role}
   const userEmail=await userCollection.findOne(query)
   if(userEmail){
        return res.send("Already register")
   }
-  const userData=await userCollection.insertOne(user)
+  const userData=await userCollection.insertOne(userAllData)
   res.send("New user create")
 })
 
